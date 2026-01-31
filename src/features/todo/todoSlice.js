595f3d183e903e8e5aa913f25614ade58e63825f
Ayush Rawat <ayushrawat75847@gmail.com>
2026-01-31 20:57:18 +0530

    add reducers

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const initialState= {
+    todos: [{id: "123", task: "demo-task", isDone: flase}]
+};
+
+export const todoSlice= createSlice({
+    name: "todo",
+    initialState,
+    reducers:{
+        addTask :(state,action)=>{
+           const newTodo= {
+            id: nanoid(),
+            task: action.payload,
+            isDone: false
+           }
+
+           state.todos.push(newTodo);
+        },
+
+        deleteTask: (state,action)=>{
+            state.todos= state.todos.filter((todo)=> todo.id !== action.payload  )
+        },
+
+        markAsDone: (state,action)=>{
+            state.todos= state.todos.map((todo)=>{
+                if(todo.id === action.payload){
+                    todo.isDone= true;
+                }
+            });
+        }  
+    }
+});
+
+export const {addTask, deleteTask, markAsDone}= todoSlice.actions;
+export default todoSlice.reducer;
