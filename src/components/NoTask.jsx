bc721a05f6c11de94c9adb6b3e59e4955451daba
Ayush Rawat <ayushrawat75847@gmail.com>
2026-01-31 23:34:03 +0530

    add delete and completed feature

--- src/components/NoTask.jsx
+++ src/components/NoTask.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 export default function NoTask() {
     const todos = useSelector((state) => state.todos);
     
-
     return (
         <>
             {todos.length ===0 && <h2>No Task Added!</h2>}
